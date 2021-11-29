bb9b620df0a7d3a9f846f614063fe356c01588aa
yellowbeee <709341218@qq.com>
2021-11-29 18:43:01 +0800

    feat: 调试

---
+++ packages/dlophin/src/start.ts
@@ -0,0 +1,52 @@
+import BuildService from './buildService'
+import detect from 'detect-port'
+import chalk from 'chalk'
+import chokidar from 'chokidar'
+import type {TCliStartOptions} from './cli'
+import getBuildConfig from './utils/getBuildConfig'
+import {ChildProcess, fork} from 'child_process'
+
+const child: ChildProcess | null = null
+
+export = async (cliOptions: TCliStartOptions) => {
+  const run = () => {
+    // port
+    const port = cliOptions.port || 3000
+    // port is alive
+    const newPort = await detect(port)
+    if (newPort != port) {
+      console.log(chalk.green(` ${port} 端口已被占用，使用${newPort} 端口启动`))
+    }
+
+    // set NODE_ENV
+    process.env.NODE_ENV = 'development'
+
+    const server = new BuildService({
+      command: 'start',
+      args: {...cliOptions, port: newPort},
+    })
+
+    server.run({})
+  }
+
+  run()
+
+  const watcher = chokidar.watch(getBuildConfig(cliOptions.config || '', cliOptions.root || process.cwd()), {
+    ignoreInitial: true,
+  })
+
+  watcher.on('change', function () {
+    console.log('\n')
+    console.info(chalk.green(`build config has been changed`))
+    console.info(chalk.green('restart dev server'))
+    // add process env for mark restart dev process
+    // process.env.RESTART_DEV = true
+    child && (child as any).kill()
+    run()
+  })
+
+  watcher.on('error', error => {
+    log.error('fail to watch file', error)
+    process.exit(1)
+  })
+}
---
+++ packages/dlophin/src/webpack/service/start.ts
@@ -0,0 +1,66 @@
+import chalk from 'chalk'
+import webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
+import openBrowser from 'react-dev-utils/openBrowser'
+import serverUrl from '../../utils/serverUrl'
+import BuildCore, {TBuildModule} from '../../core'
+
+export = async (core: BuildCore, options: any) => {
+  const {command, commandArgs, userConfig, webpackConfig, applyHook} = core
+  // devServerConfig
+  const devServerConfig: any = {
+    port: commandArgs.port || 3333,
+    host: commandArgs.host || '0.0.0.0',
+    https: commandArgs.https || false,
+  }
+
+  let compiler
+  try {
+    // do optimize
+    compiler = webpack(webpackConfig.toConfig())
+  } catch (err) {
+    console.error(chalk.red('WEBPACK', 'Failed to init webpack'))
+    await applyHook('initError', {err})
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  }
+
+  compiler.hooks.done.tap('compileHook', async stats => {
+    const statsJson = stats.toJson({
+      all: false,
+      errors: true,
+      warnings: true,
+      timings: true,
+    })
+  })
+
+  // protocol
+  const protocol = devServerConfig.https ? 'https' : 'http'
+  const localUrl = serverUrl(protocol, devServerConfig.host, devServerConfig.port)
+
+  // dev-server
+  const startDevServerTime = Date.now()
+  const server = new WebpackDevServer(devServerConfig, compiler as any) as any
+
+  server.startCallback((error?: Error) => {
+    if (error) {
+      console.log('WEBPACK', chalk.red('[ERR]: Failed to start webpack-dev-server \n'))
+      console.error('WEBPACK', error.stack || error.toString())
+    }
+
+    console.log()
+    console.log(
+      // eslint-disable-next-line node/no-missing-require
+      chalk.cyan(`\n  dlophin v${require('dlophin/package.json').version}`) + chalk.green(' Starting the server at:'),
+    )
+    console.log('   - Local  : ', chalk.underline.yellow(localUrl))
+    console.log('   - Network: ', chalk.underline.yellow(localUrl))
+    console.log(chalk.cyan(`\n  Server ready in ${Date.now() - startDevServerTime}ms.\n`))
+    console.log()
+
+    // open browser
+    if (commandArgs.open) {
+      openBrowser(localUrl)
+    }
+  })
+}
